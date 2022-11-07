2c881c8d3667e0d4406a4acba4dcb9afeb37dfa8
Adrien Dudon <c00278154@itcarlow.ie>
2022-11-07 14:14:03 +0000

    Initial commit - Entities can collect item from the ground and store them in their Inventory

---
+++ Assets/Scripts/InventorySystem/Item.cs
@@ -0,0 +1,27 @@
+﻿using InventorySystem.Interfaces;
+using InventorySystem.ScriptableObjects;
+using UnityEngine;
+
+namespace InventorySystem
+{
+    public class Item : MonoBehaviour, ICollectable
+    {
+        [SerializeField] private ItemSO itemData;
+
+        private SpriteRenderer _spriteRenderer;
+
+        private void Start()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            
+            if (_spriteRenderer != null)
+                _spriteRenderer.sprite = itemData.Icon;
+        }
+
+        public void Collect(Inventory inventory)
+        {
+            inventory.AddItem(itemData);
+            Destroy(gameObject);
+        }
+    }
+}
