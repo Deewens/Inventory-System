679c66f5293544b6519215c21b0ba8fbfe4299c1
Adrien <dudonadrien@gmail.com>
2023-04-29 12:16:28 +0100

    XK-46: A slotted inventory can be created from editor

---
+++ Packages/me.deewens.2d.inventorysystem/Runtime/InventorySlottedSO.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InventorySystem
+{
+    [CreateAssetMenu(fileName = "SlottedInventory", menuName = "Inventory System/Slotted Inventory", order = 0)]
+    public class InventorySlottedSO : ScriptableObject
+    {
+        public event Action InventoryChanged;
+
+        public event Action<ItemSO> ItemAdded;
+        public event Action<ItemSO> ItemRemoved;
+
+        public void AddItem(ItemSO itemData)
+        {
+            // if (itemData.IsStackable)
+            // {
+            //     InventoryItem item = Slots.Find(slot => slot.Item.ItemData == itemData);
+            //     if (item == null)
+            //     {
+            //         Items.Add(new InventoryItem(itemData));
+            //     }
+            //     else
+            //     {
+            //         item.Quantity++;
+            //     }
+            // }
+            // else
+            // {
+            //     Items.Add(new InventoryItem(itemData));
+            // }
+            //
+            // // Invoke event whenever an item is added
+            // ItemAdded?.Invoke(itemData);
+            //
+            // InventoryChanged?.Invoke();
+        }
+
+        public void RemoveItem(ItemSO itemData)
+        {
+            // InventoryItem item = Items.Find(item => item.ItemData == itemData);
+            //
+            // if (item != null)
+            // {
+            //     if (itemData.IsStackable)
+            //     {
+            //         item.Quantity--;
+            //         if (item.Quantity == 0)
+            //         {
+            //             Items.Remove(item);
+            //         }
+            //     }
+            //     else
+            //     {
+            //         Items.Remove(item);
+            //     }
+            //
+            //
+            //     ItemRemoved?.Invoke(itemData);
+            //     InventoryChanged?.Invoke();
+            // }
+        }
+
+        /// <summary>
+        /// Empty the inventory from every items
+        /// </summary>
+        public void Clear()
+        {
+            //Slots.Clear();
+
+            InventoryChanged?.Invoke();
+        }
+
+        public void Save()
+        {
+            string json = JsonUtility.ToJson(this);
+            FileManager.WriteToFile("inventory.json", json);
+            Debug.Log("Inventory has been saved!");
+            Debug.Log(Application.persistentDataPath);
+        }
+
+        public void Load()
+        {
+            FileManager.LoadFromFile("inventory.json", out string json);
+            JsonUtility.FromJsonOverwrite(json, this);
+            Debug.Log("Inventory has been loaded!");
+        }
+    }
+}
